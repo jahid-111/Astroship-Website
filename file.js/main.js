a7e00f17897468c6a77930281d9ffd43d109d459
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2023-12-31 22:58:46 +0300

    AddHomeNav

--- file.js/main.js
+++ file.js/main.js
@@ -1,6 +1,42 @@
 console.log("im from file js");
 
 
+
+document.getElementById('home').addEventListener('click', function(){
+ 
+    const pricePage = document.getElementById('pricePage');
+    pricePage.style.display = "none";
+
+
+    const mainSection = document.getElementById('mainSection');
+    mainSection.style.display = "none"
+    const header = document.getElementById('header');
+    header.style.display = "block";
+
+
+    const about = document.getElementById('aboutSection');
+    about.style.display = "none";
+
+    const home = document.getElementById('mainSection');
+    home.style.display = "block"
+
+
+
+
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById('pricing').addEventListener('click', function(){
  
     const pricePage = document.getElementById('pricePage');
@@ -10,11 +46,17 @@ document.getElementById('pricing').addEventListener('click', function(){
     const mainSection = document.getElementById('mainSection');
     mainSection.style.display = "none"
     const header = document.getElementById('header');
-    header.style.display = "none"
+    header.style.display = "none";
+
+
+    const about = document.getElementById('aboutSection');
+    about.style.display = "none"
 
     console.log("Pricing Section")
 
 })
+
+
 document.getElementById('about').addEventListener('click', function(){
  
     const pricePage = document.getElementById('pricePage');
